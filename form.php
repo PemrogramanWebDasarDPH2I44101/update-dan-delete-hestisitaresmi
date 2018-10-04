19cf0358a5ebc01436c0ea7d2d359546996ffc32
hestisitaresmi <42725278+hestisitaresmi@users.noreply.github.com>
2018-10-04 23:27:34 +0700

    Update form.php

--- form.php
+++ form.php
@@ -1,13 +1,38 @@
 <!DOCTYPE html>
-    <head>
-        <title></title>
-    </head>
-    <body>
-        <form action="submit.php" method="post">
-            Nama<input type="text" name="nama" id=""><br>
-            Nim<input type="text" name="nim" id=""><br>
-            Tanggal Lahir<input type="date" name="tgl_lahir" id=""><br>
-            <input type="submit" value="Kirim">
-        </form>
-    </body>
+<html>
+<head>
+	<title>FORM</title>
+</head>
+<body>
+	<table align="center" bgcolor="lightgrey" align="center"  style="border:5px double black;">
+		<form action="conn.php" colspan="3" method="POST" ali>
+			<tr>
+				<td align="center" colspan="3">LOGIN</td><br>
+			</tr>
+			<tr>
+				<td>Id</td>
+				<td>:</td>
+				<td><input type="text" name="id"></td>
+			</tr>
+			<tr>
+				<td>Nama</td>
+				<td>:</td>
+				<td><input type="text" name="nama"></td>
+			</tr>
+			<tr>
+				<td>Nim</td>
+				<td>:</td>
+				<td><input type="text" name="nim"></td>
+			</tr>
+			<tr>
+				<td>Tgl Lahir</td>
+				<td>:</td>
+				<td><input type="date" name="tgal_lahir"></td>
+			</tr>
+			<tr>
+				<td colspan="3" align="center"><input type="submit" name="submit" value="KIRIM"></td>
+			</tr>
+		</form>
+	</table>
+</body>
 </html>
